a4cbc6108e7ec9f5bb2d9aa8c519bb85db439b57
Luís Rodrigues <lfrodrigues@gmail.com>
2016-02-28 09:11:56 +0000

    Updated static validate backend to include bandit

--- static_validate_backend.sh
+++ static_validate_backend.sh
@@ -2,3 +2,6 @@
 
 # run python static validation
 prospector  --profile-path=. --profile=.prospector.yml --path=src --ignore-patterns=static
+
+# run bandit - A security linter from OpenStack Security
+bandit -r src
