28e258cfe729e82e58250b9b5d549ebbb92ef460
Luís Rodrigues <lfrodrigues@gmail.com>
2016-05-02 11:00:01 +0100

    Updated parameter for saas-lint

--- test_shippable_frontend.sh
+++ test_shippable_frontend.sh
@@ -6,7 +6,7 @@ set -e
 ./node_modules/.bin/eslint -c .eslintrc -f junit src/static tests/js > shippable/testresults/eslint.xml
 
 # scss static analysis
-./node_modules/.bin/sass-lint -f junit --verbose > shippable/testresults/sasslint.xml
+./node_modules/.bin/sass-lint -f junit --verbose --no-exit > shippable/testresults/sasslint.xml
 
 # javascript tests
 ./node_modules/.bin/istanbul cover --report cobertura --dir shippable/codecoverage node_modules/mocha/bin/_mocha -- --require tests/require --recursive --compilers js:babel-core/register --reporter mocha-junit-reporter --reporter-options mochaFile=shippable/testresults/mocha.xml tests/js/
