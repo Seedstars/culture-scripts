4d76149078a2d70c0c552e6b94c46deef32431d7
Pedro Gomes <pedrorodriguesgomes@gmail.com>
2016-08-25 16:52:39 +0100

    Fix tests folder.

--- test_local_backend.sh
+++ test_local_backend.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# Note: when first run this script or migrations are updated, please remove the options --nomigrations and --reuse-db
 
-py.test -n 8 --nomigrations --reuse-db --cov=src --cov-report=html tests/python/
+py.test -n 8 --nomigrations --reuse-db --cov=src --cov-report=html tests/
