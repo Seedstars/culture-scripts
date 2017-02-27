c43f571f7b01a5d2c43db11fc6d5e8ee826ac439
Luis Rodrigues <lfrodrigues@gmail.com>
2017-02-27 17:27:03 +0000

    Updated CI scripts to gitlab

--- notify_slack.sh
+++ notify_slack.sh
@@ -4,15 +4,15 @@
 # If it fails with permission denied, make sure you run `chmod u+x notify_slack.sh` first.
 
 STATUS=$1
-PROJECT=$2
-BUILD_NUMBER=$3
-BRANCH=$4
-URL=$5
-SLACK_URL=$6 # This should be a long URL like https://hooks.slack.com/services/AAAAA/BBBB/CCCC.
-
+GROUP=$2
+PROJECT=$3
+PIPELINE_ID=$4
+BRANCH=$5
+URL=$6
+SLACK_URL=$7 # This should be a long URL like https://hooks.slack.com/services/AAAAA/BBBB/CCCC.
 
 if [ "$STATUS" = "pass" ]; then
-   curl -X POST --data-urlencode 'payload={"username": "happyshippy", "text": "PASSING '"$PROJECT"'/'"$BRANCH"' (Build '"$BUILD_NUMBER"'). --> '"$URL"'", "icon_emoji": ":sunglasses:"}' $SLACK_URL
+   curl -X POST --data-urlencode 'payload={"username": "happyshippy", "text": "PASSING '"$GROUP"'/'"$PROJECT"'/'"$BRANCH"' (Pipeline '"$PIPELINE_ID"'). --> '"$URL"'", "icon_emoji": ":sunglasses:"}' $SLACK_URL
 else
-   curl -X POST --data-urlencode 'payload={"username": "sadshippy", "text": "FAILING '"$PROJECT"'/'"$BRANCH"' (Build '"$BUILD_NUMBER"'). --> '"$URL"'", "icon_emoji": ":scream:"}' $SLACK_URL
+   curl -X POST --data-urlencode 'payload={"username": "sadshippy", "text": "FAILING '"$GROUP"'/'"$PROJECT"'/'"$BRANCH"' (Piepleine '"$PIPELINE_ID"'). --> '"$URL"'", "icon_emoji": ":scream:"}' $SLACK_URL
 fi
