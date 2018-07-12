45dcf316707ab06e2ed8335cb88f8a14e80ba615
Luis Rodrigues <lfrodrigues@gmail.com>
2018-07-12 09:32:37 +0630

    Added black code style validation

--- static_validate_backend.sh
+++ static_validate_backend.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# run black - make sure everyone uses the same python style
+black --skip-string-normalization --line-length 120 --check tests
+black --skip-string-normalization --line-length 120 --check src
+
 # run python static validation
 prospector  --profile-path=. --profile=.prospector.yml --path=src --ignore-patterns=static
 
--- test_ci_backend.sh
+++ test_ci_backend.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# run black - make sure everyone uses same python style
+black --skip-string-normalization --line-length 120 --check tests
+black --skip-string-normalization --line-length 120 --check src
+
 # run bandit - A security linter from OpenStack Security
 bandit -r src
 
