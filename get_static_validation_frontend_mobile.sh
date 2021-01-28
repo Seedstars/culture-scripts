775fb3e7220b41b57d3bbd71acb647c61d039e45
Luis Rodrigues <lr@seedstars.com>
2021-01-28 13:03:04 +0000

    Updated to user new settings

--- get_static_validation_frontend_mobile.sh
+++ get_static_validation_frontend_mobile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2020.04/eslintrc > .eslintrc
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2020.04/prettierrc > .prettierrc
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.01/eslintrc > .eslintrc
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.01/prettierrc > .prettierrc
