641edbaf79f4a5e4450f544681b16d6478d80799
Luis Rodrigues <lr@seedstars.com>
2021-04-07 16:52:46 +0100

    Added type script static validation for new mobile apps

--- get_static_validation_frontend_mobile.sh
+++ get_static_validation_frontend_mobile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.01/eslintrc > .eslintrc
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.01/prettierrc > .prettierrc
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.04/tsconfig.json > tsconfig.json
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.04/prettierrc.js > .prettierrc.js
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.04/eslintrc.js > .eslintrc.js
--- static_validate_frontend_mobile.sh
+++ static_validate_frontend_mobile.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# validate static analsys rules from semgrep
+semgrep --config=r/typescript  src/
+semgrep --config=r/javascript  src/
+
 # validate eslint
-./node_modules/.bin/eslint -c .eslintrc -f compact js tests/js 
+./node_modules/.bin/eslint -c .eslintrc.js  -f compact 'src/**/*.{tsx,ts}'
 
 # finally valitate prettier format
-./node_modules/.bin/prettier --config .prettierrc --check "js/**/*.js"
-./node_modules/.bin/prettier --config .prettierrc --check "tests/js/**/*.js"
+./node_modules/.bin/prettier --config .prettierrc.js --check 'src/**/*.{tsx,ts}'
