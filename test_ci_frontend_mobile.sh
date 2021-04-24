dd6ff02b3db3cc469fdc029a485aecd2064ec1d1
Luís Rodrigues <lfrodrigues@gmail.com>
2021-04-24 19:51:21 +0100

    Added error in semgrep ci

--- test_ci_frontend_mobile.sh
+++ test_ci_frontend_mobile.sh
@@ -3,8 +3,8 @@
 set -e
 
 # validate static analsys rules from semgrep
-semgrep --config=r/typescript  src/
-semgrep --config=r/javascript  src/
+semgrep --error --config=r/typescript  src/
+semgrep --error --config=r/javascript  src/
 
 # validate eslint
 ./node_modules/.bin/eslint -c .eslintrc.js  -f compact 'src/**/*.{tsx,ts}'
