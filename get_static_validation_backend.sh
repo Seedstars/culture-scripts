4f3f3e3d78bd0498e65fc22fb0c8e213e01238ec
Hugo Rodrigues <hugo@quickcheck.ng>
2021-08-31 12:26:29 +0100

    Updated prospector rules

--- get_static_validation_backend.sh
+++ get_static_validation_backend.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.01/prospector.yml > .prospector.yml
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.01/coveragerc > .coveragerc
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.09/prospector.yml > .prospector.yml
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.09/coveragerc > .coveragerc
