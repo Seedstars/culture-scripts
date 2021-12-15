01d2eebb055a318576798c0263d9ca7bd9ebf7aa
fernandogomes <fernandogomes90@gmail.com>
2021-12-15 15:10:40 +0000

    update mobile scripts to 2021.12

--- get_static_validation_frontend_mobile.sh
+++ get_static_validation_frontend_mobile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.06/tsconfig.json > tsconfig.json
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.06/prettierrc.js > .prettierrc.js
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.06/eslintrc.js > .eslintrc.js
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.12/tsconfig.json > tsconfig.json
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.12/prettierrc.js > .prettierrc.js
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2021.12/eslintrc.js > .eslintrc.js
