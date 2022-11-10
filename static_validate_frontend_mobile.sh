11b738d25bb8aa02be936a93d59f4ddfe8fd656d
fernandogomes <fernandogomes90@gmail.com>
2022-11-10 12:41:47 +0000

    point to new 2022/11 rules

--- static_validate_frontend_mobile.sh
+++ static_validate_frontend_mobile.sh
@@ -6,5 +6,8 @@ semgrep --strict --error --config .semgrep_rules.yml src/
 # validate eslint
 ./node_modules/.bin/eslint -c .eslintrc.js  -f compact 'src/**/*.{tsx,ts}'
 
-# finally valitate prettier format
+# validate prettier format
 ./node_modules/.bin/prettier --config .prettierrc.js --check 'src/**/*.{tsx,ts}'
+
+# validate ts typing 
+./node_modules/.bin/tsc --noEmit --project tsconfig.json
