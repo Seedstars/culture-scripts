6891ac744a2c8996130559f84f5302eaef4f947f
Luís Rodrigues <lfrodrigues@gmail.com>
2022-11-16 14:21:19 +0400

    Updated get_static_validation_backend.sh to use 2022.11 version

--- get_static_validation_backend.sh
+++ get_static_validation_backend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.07/prospector.yml > .prospector.yml
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.07/coveragerc > .coveragerc
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.11/prospector.yml > .prospector.yml
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.11/coveragerc > .coveragerc
 
-python scripts/semgrep_rules.py -r python -v 2022.07
+python scripts/semgrep_rules.py -r python -v 2022.11
