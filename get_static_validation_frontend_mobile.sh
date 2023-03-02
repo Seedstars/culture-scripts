3433a7c4ba81ad57885c4732d7947db4e8dc3368
danielsaraiva <daniel@quickcheck.ng>
2023-03-02 17:46:43 +0000

    Update get static validation for RN 0.71

--- get_static_validation_frontend_mobile.sh
+++ get_static_validation_frontend_mobile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.11/tsconfig.json > tsconfig.json
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.11/prettierrc.js > .prettierrc.js
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.11/eslintrc.js > .eslintrc.js
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2023.03/tsconfig.json > tsconfig.json
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2023.03/prettierrc.js > .prettierrc.js
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2023.03/eslintrc.js > .eslintrc.js
 
 python scripts/semgrep_rules.py -r javascript typescript -v 2022.07
