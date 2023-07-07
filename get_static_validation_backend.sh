68343a133866ef1a623c30db4e45c7b793d12346
Luís Rodrigues <lfrodrigues@gmail.com>
2023-07-07 18:55:04 +0800

    Updated versions

--- get_static_validation_backend.sh
+++ get_static_validation_backend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.11/prospector.yml > .prospector.yml
-curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2022.11/coveragerc > .coveragerc
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2023.03/prospector.yml > .prospector.yml
+curl https://raw.githubusercontent.com/Seedstars/culture/master/code/validation/2023.03/coveragerc > .coveragerc
 
-python scripts/semgrep_rules.py -r python -v 2022.11
+python scripts/semgrep_rules.py -r python -v 2023.03
